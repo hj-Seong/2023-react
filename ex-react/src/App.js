f34c20701462ae84ba66ca3705d2f2cd2d2a17d1
HJ S <hjseong1222@gmail.com>
2023-03-30 18:15:58 +0900

    0330 컴포넌트 props state 확인

--- ex-react/src/App.js
+++ ex-react/src/App.js
@@ -1,20 +1,23 @@
 import './App.css';
+import PropsComp from './components/PropsComp';
+import CountComp from './components/CountComp';
+import CountPropsComp from './components/CountPropsComp';
 
 function App() {
   return (
     <div className="App">
-      {/** props을 사용하는 클래스컴포넌트 */}
-      <PropsComp color="red">
+      {/** props을 사용하는 클래스컴포넌트 작성*/}
+      <PropsComp color="blue">
         props값을 받아와서 글자색을 바꿉니다
       </PropsComp>
-      {/** state을 사용하는 클래스컴포넌트
+      {/** state을 사용하는 클래스컴포넌트 작성
        * 버튼을 클릭할때마다 10씩 증가하는 컴포넌트
        */}
       <CountComp />
-      {/** props, state을 사용하는 클래스컴포넌트 
+      {/** props, state을 사용하는 클래스컴포넌트 작성
        * props의num값을 가져와서 버튼을 클릭할때마다 num씩증가
       */}
-      <CountPropsComp num={5} />
+      <CountPropsComp num={20} />
     </div>
   );
 }
