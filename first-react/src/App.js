b6a01716d6c28ceea04cf68f166b40fba064258d
HJ S <hjseong1222@gmail.com>
2023-03-30 10:10:09 +0900

    0330 react확인

---
+++ first-react/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>리액트 프로젝트를 수정해서 사용합니다</h1>
+    </div>
+  );
+}
+
+// import를 이용하여 다른 파일에서 값을 가져올때
+// export를 이용해서 내보내는 값
+export default App;
