72935d8cea5e3f37b2760230808d38e49839b571
HJ S <hjseong1222@gmail.com>
2023-03-30 10:58:20 +0900

    0330 컴포넌트1

--- first-react/src/App.js
+++ first-react/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import MyComponent from './components/MyComponent';
 
 function App() {
   return (
     <div className="App">
       <h1>리액트 프로젝트를 수정해서 사용합니다</h1>
+      <MyComponent />
     </div>
   );
 }
