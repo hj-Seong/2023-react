5a1a2d72cbac972bd8f098d1411be3fc76e22845
HJ S <hjseong1222@gmail.com>
2023-04-04 11:22:09 +0900

    0404 라이프사이클 - 업데이트 확인

--- first-react/src/components/LifeCycle.jsx
+++ first-react/src/components/LifeCycle.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 
+// 클래스형 컴포넌트에서 라이프 사이클 작성
 export class LifeCycle extends Component {
   constructor(props) {
     super(props);
@@ -8,18 +9,29 @@ export class LifeCycle extends Component {
         date : new Date()
     }
   }
+
+  // 라이프 사이클 메소드 - 마운트 되었을때: 시작했을 때 실행
+  // 컴포넌트가 보이는 한번만 실행
   componentDidMount(){
     console.log("마운트가 되었습니다");
-    // setInterval(()=>{this.tick}, 1000);
+    // 처음 한번 실행하는 내용 작성
+    // 또는 외부(공공데이터)에서 값을 한 번만 가져올때
+    // setInterval(()=>{this.tick()}, 1000);
     setInterval(this.tick, 1000);
   }
+
+  // 라이프 사이클 메소드 : 업데이트가 되었을때 : 화면이 바뀌었을때
+  // 현재 컴포넌트가 업데이트 될때마다 실행 
+  // props의 값이 바뀔때, setState를 통해서 값이 바뀔때
   componentDidUpdate() {
+    // 모든 업데이트 마다 실행할 내용 작성가능
     console.log("업데이트가 되었습니다");
   }
   componentWillUnmount() {
     console.log("컴포넌트가 언마운트 되었습니다")
   }
 
+  // 화면에 시간 출력 메소드
   printClock = (time) =>{
     // 가능하면 this.setState사용X
     const hours = time.getHours();
@@ -28,7 +40,9 @@ export class LifeCycle extends Component {
     return `${hours}:${minutes}:${seconds}`
   }
 
+  // 시간값을 다시 할당 메소드
   tick = () => {
+    // setState를 사용할때 마다 업데이트 발생
     this.setState({date : new Date()})
   }
 
