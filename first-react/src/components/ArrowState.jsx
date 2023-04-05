0460e8e52bf16739d18f5be8d10058cd41f334b4
HJ S <hjseong1222@gmail.com>
2023-04-05 13:07:46 +0900

    0405 함수형 컴포넌트 state에 따른 디자인

--- first-react/src/components/ArrowState.jsx
+++ first-react/src/components/ArrowState.jsx
@@ -1,6 +1,9 @@
 // 16.8버전 이후로 함수형에서 state 사용 가능
 import { useState } from "react";
 
+// css파일 들고오기
+import './arrowState.css'
+
 // 화면의 업데이트에 상관없이 사용하는 변수
 // 현재 컴포넌트에서 전역변수로 쓸 변수
 let globalId = 4;
@@ -23,9 +26,9 @@ const ArrowState = (props) => {
     // map을 이용하여 id값과 name을 화면에 출력
     const [students, setStudents] = useState(
         [
-            {id: 1, name:"홍길동"},
-            {id: 2, name:"성춘향"},
-            {id: 3, name:"흥부"},
+            {id: 1, name:"홍길동", checked:true },
+            {id: 2, name:"성춘향", checked:false },
+            {id: 3, name:"흥부",  checked:false },
         ]
     );
     // 학생 이름을 받아올 공간
@@ -52,6 +55,16 @@ const ArrowState = (props) => {
         globalId++;
         setStudents(newStudents);
         setInputName("");
+    }
+
+    // id값을 전달하여 메소드 안에서 사용
+    const deleteStudent = (id)=>{
+        // 클릭한 id값을 제외하고 새로운 배열작성
+        // filter
+        const newStudents = students.filter(
+            (s)=>s.id !== id
+        )
+        setStudents(newStudents);
     }
 
     return (
@@ -86,8 +99,43 @@ const ArrowState = (props) => {
             <ul>
                 {
                     students.map((student)=>
-                        <li key={student.id}>
+                        <li 
+                            key={student.id}
+                            className={ student.checked ? "on" : "" }
+                        >
+                            <input type="checkbox" 
+                                checked={student.checked}
+                                readOnly
+                                onClick={()=>{
+                                    // 체크박스를 클릭하면, 클릭한 객체의 checked 값이 바뀜
+                                    // map을 이용하여 작성
+                                    // map() : 배열안의 요소의 값을 return을 통해 새로운 배열만듬
+                                    // 클릭한 객체를 찾았다면 > 그객체의 checked 값을 수정해서 return
+                                    const newStudents = students.map((s)=>{
+                                        // s통해서 각각의 객체값 확인
+                                        // 1) 클릭한 체크박스의 id값과 모든 s의id와 비교
+                                        // 2) id 값이 같지않다면 원래 객체 
+                                        // 3) id 값이 같다면 checked 값을 !을 이용하여 수정한 객체
+                                        if(student.id !== s.id) {
+                                            return s;
+                                        } else {
+                                            // 원래 객체에서 checked 값만 수정하기 위해서
+                                            // s안에있는 속성을 ...(스프레드 연산자)를 이용해서 추가
+                                            // 수정할 속성인 checked를 작성해서 수정
+                                            return {...s, checked: !s.checked }
+                                        }
+
+                                    })
+                                    setStudents(newStudents);
+                                }}
+                            />
                             {student.id} , {student.name}
+                            <button
+                                // 클릭했을때 배열 삭제 > 클래스형 컴포넌트 내용 참고
+                                onClick={()=>{deleteStudent(student.id)}}
+                            >
+                                X
+                            </button>
                         </li>
                     )
                 }
