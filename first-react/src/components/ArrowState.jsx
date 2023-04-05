060d4850df8cec3738074fd8d28e700e2409722e
HJ S <hjseong1222@gmail.com>
2023-04-05 12:05:46 +0900

    0405 함수형 컴포넌트 이벤트와 메소드

--- first-react/src/components/ArrowState.jsx
+++ first-react/src/components/ArrowState.jsx
@@ -1,6 +1,14 @@
 // 16.8버전 이후로 함수형에서 state 사용 가능
 import { useState } from "react";
 
+// 화면의 업데이트에 상관없이 사용하는 변수
+// 현재 컴포넌트에서 전역변수로 쓸 변수
+let globalId = 4;
+
+
+// 함수형 컴포넌트의 특징
+// 1) this를 사용하지 않는다
+// 2) 안에 값을 넣어줄때 변수로 할당하므로 const나 let 사용
 const ArrowState = (props) => {
     // useState는 항상 함수(컴포넌트)안에 작성
     const [message, setMessage] = useState(
@@ -20,6 +28,31 @@ const ArrowState = (props) => {
             {id: 3, name:"흥부"},
         ]
     );
+    // 학생 이름을 받아올 공간
+    const [inputName, setInputName] = useState("");
+    
+    
+    // 클래스형에서 render()안에 값이 계속 초기화
+    // 함수형 클래스에서는 함수 안에 있는 내용이 계속 초기화
+    let id = 4;
+
+
+    // 메소드 작성 
+    const inputChange = (e)=>{setInputName(e.target.value)};
+
+    const addStudent = () => {
+        // 값을 받아와서 새로운 배열로 만들기
+        // 새로운 배열 students 할당
+        const newStudents = students.concat(
+            {
+                id : globalId,
+                name : inputName
+            }
+        );
+        globalId++;
+        setStudents(newStudents);
+        setInputName("");
+    }
 
     return (
         <div>
@@ -39,6 +72,27 @@ const ArrowState = (props) => {
             {
                 array.map((num,i)=><li key={i}>{num}</li>)
             }
+            <h3>학생추가</h3>
+            <input 
+                type="text"
+                onChange={inputChange}
+                value={inputName}
+            /> 
+            <button
+                onClick={addStudent}
+            >
+                추가
+            </button>
+            <ul>
+                {
+                    students.map((student)=>
+                        <li key={student.id}>
+                            {student.id} , {student.name}
+                        </li>
+                    )
+                }
+            </ul>
+
         </div>
     )
 }
