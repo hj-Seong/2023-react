4c57162749c4ea04a326b160a964334e494dcd61
HJ S <hjseong1222@gmail.com>
2023-04-07 15:13:17 +0900

    0407 todolist-1

---
+++ 0407-todo/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TodoComp from './components/TodoComp';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoComp />
+    </div>
+  );
+}
+
+export default App;
