6239c8af1a1094e6a474ff067af12575d2e36bcf
HJ S <hjseong1222@gmail.com>
2023-04-11 16:08:49 +0900

    0411 리액트 라우터-useSearchParams

--- router-app/src/page/Home.jsx
+++ router-app/src/page/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 // Link를 통해서 react-router-dom에서 지정한 주소로 이동
 import { Link } from 'react-router-dom'
+import FormComp from '../components/FormComp';
 
 export default function Home() {
   const fruit = "banana";
@@ -14,18 +15,25 @@ export default function Home() {
         <Link to="/about">About</Link>
         {/** to의 속성값으로 자바스크립의 문자열 가능 */}
         <Link to={`/story/${fruit}`}>Story</Link>
+
+        <FormComp />
+
         
         <hr />
         {/** map을 이용해서 배열의값을 Link의 to 주소값으로 사용 */
           fruits.map((f, i)=>(
-            <Link to={`/story/${f}`} key={i}> [{f}Story] </Link>
+            <Link to={`/story/${f}`} key={i}> | {f}Story | </Link>
           ))
         }
         <hr />
         {/** fruits의 map을 사용하여
          * /story2/apple, /story2/orange, story2/peach 로 이동하는
          * Link 작성
-         */}
+         */
+          fruits.map((f)=>(
+            <Link to={`story2/${f}`}>| {f}Stroy2 |</Link>
+          ))
+        }
     </div>
   )
 }
