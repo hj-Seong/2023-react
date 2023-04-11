ab7b374e79c7f3d156000ac5171b38c1ee33400a
HJ S <hjseong1222@gmail.com>
2023-04-11 11:36:15 +0900

    0411 리액트 라우터-1

---
+++ router-app/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+// 라우터를 가져오기위해 router-dom에서 컴포넌트 가져옴
+import { Routes, Route } from 'react-router-dom';
+
+// 각각의 페이지(하나에 주소에 보일 컴포넌트)
+import Home from './page/Home';
+import About from './page/About';
+
+function App() {
+  return (
+    // Routes를 이용하여 컴포넌트와 주소를 연결하는 Route를 정리
+    // div안에 Routes를 넣어서 사용해도 상관없다
+    <Routes>
+      {/* app.js 안에서 Route 이용해서 주소와 컴포넌트 연결
+        * path : 주소 '/' : 첫 화면에 보이는 주소
+        * element : 컴포넌트 { <Home/> }를 통해서 전달 
+       */}
+      <Route path='/' element={<Home />} />
+      <Route path='/about' element={ <About /> } />
+    </Routes>
+  );
+}
+
+export default App;
---
+++ router-app/src/page/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+        <h1>Home</h1>
+        <p>현재 화면은 홈입니다.</p>
+    </div>
+  )
+}
