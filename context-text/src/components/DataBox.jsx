11c18b06897746e3ea4481d838659d83433e5cba
HJ S <hjseong1222@gmail.com>
2023-04-14 17:15:55 +0900

    0414 Context-실습2확인

--- context-text/src/components/DataBox.jsx
+++ context-text/src/components/DataBox.jsx
@@ -18,7 +18,7 @@ function DataText () {
 
     // input 태그의 값 받아와서 input에 저장
     // 버튼을 눌렀을때 Context에 있는 이름이 바뀌게 작성
-    const [input, setInput] =useState("")
+    const [input, setInput] = useState("")
     
     // value 값이 null 또는 값
     // null일때는 false 이므로 값을 출력하지않게하고
@@ -26,8 +26,11 @@ function DataText () {
     return (
         <div>
             {value && <h3>{value.state.name}</h3>}
+            <input type="text"
+                onChange={(e)=>{setInput(e.target.value)}}
+            />
             <button 
-                onClick={()=>{value.action.setName("Green")}}
+                onClick={()=>{value.action.setName(input)}}
             > 
                 버튼을 누르면 name 수정
             </button>
