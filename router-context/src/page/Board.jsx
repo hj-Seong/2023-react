2e8357b2d816f91341d73d0d8723e08059ad27c6
HJ S <hjseong1222@gmail.com>
2023-04-19 11:50:48 +0900

    0419 코멘트 filter를 이용하여 원하는 값만 출력

--- router-context/src/page/Board.jsx
+++ router-context/src/page/Board.jsx
@@ -28,6 +28,15 @@ export default function Board() {
   // undefined의 값에 속성을 찾으려고하면 >> 오류!
   const boardData = boardlist.find((d)=>(d.id == id))
 
+  // state의 commentlist에서 boardId와 param의 id값이 같은 
+  // 새로운 배열 작성 (filter)
+  const boardCommnetlist = state.commentlist.filter(
+    (comment)=>(comment.boardId == id)
+  )
+
+
+
+
   // useEffect를 사용해서 boardData값이 undefind면 
   // 오류 페이지 컴포넌트로 이동 혹은 목록으로 이동
   // 두번쨔 인자값이 빈 배열이라면 컴포넌트 생성시에 실행
@@ -98,7 +107,9 @@ export default function Board() {
         }
         <hr />
         { // 값을 넘길 형태가 객체로 주어져있으면 객체로 넘길수 있다
-          state.commentlist.map((comment)=>(
+        // state의 commentlist를 그대로 쓰게되면 전체가 나옴
+        // >> 동일한 boardId를 가진 commentlist를 만들어야함
+          boardCommnetlist.map((comment)=>(
             <CommentComp 
               comment={comment}
             />
